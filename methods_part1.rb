37f7648e2160ce94ad102eec22a984f856bebbe6
Denis <den.scherbinin2018@yandex.ru>
2023-07-29 22:12:52 +0500

    statr learn methods

---
+++ methods_part1.rb
@@ -0,0 +1,25 @@
+# методы, часть 1
+
+name = 'Denis'
+
+def greeting(name)
+    puts "Hello, #{name}!"
+end
+
+greeting(name)
+puts '__________________________________________'
+
+def greeting_company
+    puts 'Hello, Umbrella'
+end
+
+def greetings
+    name = 'Alex Wesker'
+    greeting_company
+    greeting(name)
+end
+
+greetings # В этой функции вызываются две функции определнные выше
+
+
+
