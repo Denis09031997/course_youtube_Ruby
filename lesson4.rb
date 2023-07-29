7d9394e8bf5be545cfc46f5e34a72b44e72366b3
Denis <den.scherbinin2018@yandex.ru>
2023-07-29 11:51:39 +0500

    start learn ruby while

--- lesson4.rb
+++ lesson4.rb
@@ -1,1 +1,39 @@
 # условные выражения
+
+if 2 > 1
+  puts '2 > 1'
+end
+
+if 2 > 9
+  puts 'true'
+else
+  puts 'Ложь'
+end
+
+puts '________________________________________________'
+
+puts 'YES!' unless 2 < 1 # unless - обратный оператору if, т.е: выведи "да", если это ложь
+puts 'yes' if 2 < 1
+puts '_____________________________'
+
+name = 'Olga'
+
+if name == 'Denis'
+  puts 'My name is ' + name
+elsif name == 'Igor'
+  puts 'Ииииииииииигааааааааааааааааарь'
+elsif name == 'Olga'
+  puts 'My name is ' + name
+end
+
+puts '____________________________________'
+
+if name == 'Denis' && 2 < 4
+  puts 'Yeeeeeeeees!'
+end
+
+puts '__________________________________________________'
+
+if name == 'Denis' || 1 < 2 && name != 'Olga'
+  puts 'Good work!'
+end
